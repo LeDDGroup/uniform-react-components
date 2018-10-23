5afb0ee8bf84530e443e7ebb4fa945b52ac73112
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-10-23 17:43:02 -0400

    build: setup

---
+++ webpack-config.ts
@@ -0,0 +1,30 @@
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import { Configuration } from "webpack";
+import { resolve } from "path";
+
+const config: Configuration = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  output: {
+    filename: "bundle.js",
+    path: resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          configFile: "tsconfig-build.json"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin()]
+  }
+};
+
+export default config;
