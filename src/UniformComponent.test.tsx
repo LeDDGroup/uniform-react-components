f1958636e0bf7862fe8ce72e3447ee3b8ec08b8f
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-10-29 14:12:58 -0400

    chore: clean tests a bit

--- src/UniformComponent.test.tsx
+++ src/UniformComponent.test.tsx
@@ -4,15 +4,12 @@ import { mount } from "enzyme"
 import { UniformComponent } from "./index"
 
 describe("<UniformComponent>", () => {
-  it("should dispatch change event", () => {
-    class TestUniformComponent extends UniformComponent<{
-      bar: string
-    }> {
-      render() {
-        return <input onChange={ev => this.onChange.bar(ev.target.value)} type="string" />
-      }
+  class TestUniformComponent extends UniformComponent<{ bar: string }> {
+    render() {
+      return <input onChange={ev => this.onChange.bar(ev.target.value)} type="string" />
     }
-
+  }
+  it("should dispatch change event", () => {
     const defaultValue = { bar: "hello" }
     let changed: null | { bar: string } = null
     const wrapper = mount(
@@ -23,14 +20,6 @@ describe("<UniformComponent>", () => {
     expect(changed).toMatchObject({ bar: "foo" })
   })
   it("should allow ommiting onChange property", () => {
-    class TestUniformComponent extends UniformComponent<{
-      bar: string
-    }> {
-      render() {
-        return <input onChange={ev => this.onChange.bar(ev.target.value)} type="string" />
-      }
-    }
-
     const defaultValue = { bar: "hello" }
     const wrapper = mount(<TestUniformComponent defaultValue={defaultValue} />)
     const input = wrapper.find("input")
