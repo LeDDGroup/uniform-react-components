acf67b338b29e0cfe8ad3362c45bbd5a9b5e3251
David Perez Alvarez <david@leddgroup.com>
2018-11-08 13:55:36 -0500

    feat: use "path" from updated handle-on-change(@1.3.1)
    Allow SFC

--- src/UniformComponent.tsx
+++ src/UniformComponent.tsx
@@ -1,25 +1,37 @@
-import React, { ComponentClass /* useState  */ } from "react"
+import React, { ComponentClass, SFC } from "react"
 import Data, { IData } from "handle-data-change"
 
 export { IData }
 
 export type IProps<D, P = {}> = {
-  onChange?: (newValue: D) => void
   value: D
-  defaultValue?: D // DEPRECATED
+  onChange?: (newValue: D) => void
+  path?: string[]
 } & P
 
 export type UniformProps<D, P = {}> = {
   data: IData<D>
 } & P
 
-export function UniformComponent<D, H>(Component: ComponentClass<UniformProps<D, H>>) {
-  let onChangeFunction: (data: D) => void = () => null
-  const data = new Data<D>({} as D, (data: D) => onChangeFunction(data))
+function equal<T>(a: T, b: T) {
+  return a === b
+}
+
+export function UniformComponent<D, H>(
+  Component: ComponentClass<UniformProps<D, H>> | SFC<UniformProps<D, H>>,
+) {
+  let uniOnChange: (data: D) => void = () => null
+  let uniValue: D = {} as D
+  let uniPath: string[]
+  let data = new Data<D>({} as D, uniOnChange)
   return function<H>(props: IProps<D> & H) {
-    const { onChange, value, defaultValue, ...rest } = props as any
-    data.value = value || defaultValue
-    onChangeFunction = onChange
+    const { onChange, value, defaultValue, path, ...rest } = props as any
+    if (!equal(onChange, uniOnChange) || !equal(value, uniValue) || !equal(path, uniPath)) {
+      uniOnChange = onChange
+      uniValue = value
+      uniPath = path
+      data = new Data(value, onChange, path)
+    }
     return <Component {...rest} data={data} />
   }
 }
